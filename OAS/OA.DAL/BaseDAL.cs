e550bbf9ba35577cff76354bc26ff226a2954e2c
lizhen325 <zhenli336@gmail.com>
2016-03-26 00:37:33 +1300

    Project Created

---
+++ OAS/OA.DAL/BaseDAL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OA.Moel;
+using System.Linq.Expressions;
+
+namespace OA.DAL
+{
+    public class BaseDAL<T> where T: class,new()
+    {
+        
+    }
+}
