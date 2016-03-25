f10cf6d506d9bc788ecdc6afb542f4319a241f1c
lizhen325 <zhenli336@gmail.com>
2016-03-26 01:04:26 +1300

    Created UserInfo Model

---
+++ OAS/OA.BLL/UserInfoBLL.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OA.Moel;
+
+namespace OA.BLL
+{
+    public class UserInfoBLL:BaseBLL<UserInfo>
+    {
+
+    }
+}
---
+++ OAS/OA.DAL/UserInfoDAL.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OA.Moel;
+
+namespace OA.DAL
+{
+    public class UserInfoDAL:BaseDAL<UserInfo>
+    {
+
+    }
+}
